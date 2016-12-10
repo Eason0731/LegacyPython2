746490e92b66fb6d90107737ce7dd2713e6dc886
John0731 <john.zhang0731@outlook.com>
2016-12-10 22:42:21 +0800

    [Home]:Add the method of how to read the content of XML and delete file with specific content on XML file

--- src/Performance/DeleteFailedXmls.py
+++ src/Performance/DeleteFailedXmls.py
@@ -6,31 +6,28 @@ Created on Dec 8, 2016
 import os
 import time
 import shutil
+from xml.dom.minidom import parse
 
 def DeleteFailedXmlFiles():
-    XmlDir = r'E:\Test'
+    XmlDir = r'E:\Performance Result\2.1.4259_Yosemite_performance'
     for root,dirs,filenames in os.walk(XmlDir):
         for myFile in filenames:
             if 'xml' in myFile:
-                print "Found txt file: " + os.path.join(root,myFile)
-                xmlfiles = os.path.join(root,myFile)
-                f = open (xmlfiles)
-                content = f.readlines() #The content will print line by line
+                #print "Found xml file: " + os.path.join(root,myFile)            
+                #f = open (xmlfiles)              
+                #content = f.readlines() #The content will print line by line
                 #print content
                 
-                for eachline in content:
-                    #print eachline
-                    if '<skills>' in eachline:
-                        myXMLfiles = os.path.join(root,myFile) 
-                        f.close() #Should close file then delete
-                        print "Failed xml found on " + myXMLfiles
-                        '''
-                        os.remove(myXMLfiles)
-                        if not os.path.exists(myXMLfiles):
-                            print "Delete file " + myXMLfiles + " Successfully"
-                        '''  
-                print "==================================="
-                f.close()
+                xmlfiles = os.path.join(root,myFile)
+                xmldoc = parse(xmlfiles)
+                xmlContent = xmldoc.toxml()
+                
+                if '<Fail/>' in xmlContent:                   
+                    os.remove(xmlfiles)
+                    if not os.path.exists(xmlfiles):
+                        print "Useless file " +xmlfiles + " has been deleted successfully!"
+                        
+                #f.close()
 
 if __name__ == '__main__':
     DeleteFailedXmlFiles()
