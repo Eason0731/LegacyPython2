0d1a17753de9c222c4539f85fdc48f77f7af46d4
John0731 <john.zhang0731@outlook.com>
2017-06-15 16:25:58 +0800

    Update the logic on function 1

--- src/MyItems/FileOperation.py
+++ src/MyItems/FileOperation.py
@@ -112,20 +112,18 @@ Please choose : """)
         exit(1)
       
 def ReplaceContentOnDir(Dir):
-    Original = raw_input("Please input what word you want to find: ")
-    Replace = raw_input("Please input what word you want to replace: ")
-    t = 0
-    c = 0
-    print "================================ Start ================================"
-    print time.strftime("Start Time :%Y-%m-%d %X",time.localtime())
-    for root,dirs,filenames in os.walk(Dir):
-        for myFile in filenames:
-            TxtFile = os.path.join(root,myFile)
-            if "txt" in myFile:
-                t = t + 1  
+    if 'txt' in str(os.listdir(Dir)):
+        c = 0
+        Original = raw_input("Please input what word you want to find: ")
+        Replace = raw_input("Please input what word you want to replace: ")
+        print "================================ Start ==============================="
+        print time.strftime("Start Time :%Y-%m-%d %X",time.localtime())
+        for root,dirs,filenames in os.walk(Dir):
+            for myFile in filenames:
+                TxtFile = os.path.join(root,myFile)
                 files = open (TxtFile,'r') 
                 content = files.read()
-                files.close()         
+                files.close()
                 if Original in content:
                     c+=1         
                     files = open (TxtFile,'w') 
@@ -134,12 +132,14 @@ def ReplaceContentOnDir(Dir):
                     print Original + " has been replaced as " + Replace + " on file "+ TxtFile +" succeeded!"
                     print "   "
                 files.close()
-    if c == 0 and t != 0:
-        print Original + " didn't found on " + Dir
-    if c == 0 and t == 0:
+        
+        if c ==0:
+            print Original + " didn't found on " + Dir
+        
+        print time.strftime("End Time :%Y-%m-%d %X",time.localtime())
+        print "================================ Finish ==============================="
+    else:
         print "There is no txt files under folder " + Dir
-    print time.strftime("End Time :%Y-%m-%d %X",time.localtime())
-    print "================================ Finish ==============================="     
     CountineOrExit()
 
 def DeleteMethod(Dir):
