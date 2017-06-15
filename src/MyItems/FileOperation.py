f5d4eb87e65054f23fa9e8f5bf18a4be8293bb71
John0731 <john.zhang0731@outlook.com>
2017-06-15 15:19:58 +0800

    Update the new logic for function 5

--- src/MyItems/FileOperation.py
+++ src/MyItems/FileOperation.py
@@ -246,33 +246,30 @@ def MoveMethod(Source):
     CountineOrExit()
 
 def FindOnTxtMethod(Dir):
-    Content = raw_input ("What content you want to find on txt file? ")
-    t = 0
-    c = 0
-    print "================================ Start ==============================="
-    print time.strftime("Start Time :%Y-%m-%d %X",time.localtime())
-    for root,dirs,filenames in os.walk(Dir):
-        for myFile in filenames:
-            TxtFile = os.path.join(root,myFile)
-            i=0          
-            if 'txt' in myFile:
-                t = t + 1      
+    if 'txt' in str(os.listdir(Dir)):
+        c = 0
+        Content = raw_input ("What content you want to find on txt file? ")
+        print "================================ Start ==============================="
+        print time.strftime("Start Time :%Y-%m-%d %X",time.localtime())
+        for root,dirs,filenames in os.walk(Dir):
+            for myFile in filenames:
+                i = 0
+                TxtFile = os.path.join(root,myFile)
                 f = open (TxtFile, 'r')
-                Filecontent = f.readlines()
+                Filecontent = f.readlines()              
                 for eachline in Filecontent:
                     if Content.lower() in eachline.lower():
-                        i+= 1
-                        c+= 1      
+                        i += 1
+                        c += 1
+                f.close()
                 if i > 0:
                     print Content + " found on " + TxtFile + " for " + str(i) + " times"
-                    print "   "
-                f.close()
-    if c == 0 and t != 0:
-        print Content + " didn't found on " + Dir              
-    if c == 0 and t == 0:
-        print "There is no txt files under folder " + Dir
-    print time.strftime("End Time :%Y-%m-%d %X",time.localtime())
-    print "================================ Finish ==============================="
+        if c ==0 and i == 0:
+            print Content + " didn't found on " + Dir
+        print time.strftime("End Time :%Y-%m-%d %X",time.localtime())
+        print "================================ Finish ==============================="
+    else:
+        print "There is no txt files under folder " + Dir   
     CountineOrExit()
                 
 def FindOnDirsMethod(Dir):
