06aa87d8773c564510de670f501f8da8aa4d74c3
John0731 <john.zhang0731@outlook.com>
2017-07-07 09:26:37 +0800

    Move some codes to ViewPCInfos.py from FileOperation.py for function 9.They can call each other

--- src/MyItems/FileOperation.py
+++ src/MyItems/FileOperation.py
@@ -4,7 +4,7 @@ import re
 import shutil
 import time
 import sys
-
+import ViewPCInfos
 
 def MainFunction():
     Choose = raw_input ("""
@@ -106,7 +106,7 @@ Please choose : """)
                    ExistOrNot(Dir)
 
             if int(Choose) == 9:
-                ViewPCInfos()
+                ViewPCInfos.ViewPCInfos()
     
         else:
             print "Bye~"
@@ -339,42 +339,6 @@ def RenameWithSpecificNameMethod(Dir):
     print "================================ Finish ==============================="
     CountineOrExit()              
 
-def ViewPCInfos():
-    import getpass
-    import socket
-    import uuid
-    import platform
-    import re,urllib2
-    from subprocess import Popen, PIPE
-    output = os.popen('pip list')
-    if 'WMI' not in output.read():
-        os.system('pip install wmi')
-    pcname = socket.getfqdn(socket.gethostname())
-    currentusername = getpass.getuser()
-    mac = uuid.UUID(int = uuid.getnode()).hex[-12:]  
-    mac_address = "-".join([mac[e:e+2] for e in range(0,11,2)])
-    n_ip = re.search('\d+\.\d+\.\d+\.\d+',Popen('ipconfig', stdout=PIPE).stdout.read()).group(0)  
-    print "======================================================================="
-    os_version()
-    cpu_mem() 
-    disk()
-    print "PC name: " + pcname
-    print "Current login user: " + currentusername
-    print "Intranet IP: " + n_ip
-    print "Public network IP: " + GetPublicNetworkIP()
-    print "Mac address: " + mac_address
-    print "======================================================================="
-    CountineOrExit()
-
-def GetPublicNetworkIP():
-    import re,urllib2
-    try:
-        ipURL = urllib2.urlopen('http://ip138.com/ip2city.asp').read() #'http://ip138.com/ip2city.asp' This site is available to search IP
-        publicip = re.search('\d+\.\d+\.\d+\.\d+',ipURL).group(0)
-    except Exception,e:
-        publicip = str(e)
-    return publicip
-        
 def CountineOrExit():
     IsExit = raw_input ("Countine(Y) or Exit(N)? ")
     while(1):
@@ -395,39 +359,6 @@ def EmptyOrNot():
 def ExistOrNot(Dir):
     print "{0} is NOT Exist!" .format(Dir)
     CountineOrExit()
-
-def os_version():
-    import platform
-    import wmi
-    c = wmi.WMI ()
-    print "OS name and version: " + platform.platform()
-    for sys in c.Win32_OperatingSystem(): 
-        #print "OS name and version: %s" % sys.Caption.encode("UTF8"),"Vernum:%s" % sys.BuildNumber 
-        print "Bits: " + sys.OSArchitecture.encode("UTF8")
-        print "Current process count: " + str(sys.NumberOfProcesses)
-    print "       "
-
-def cpu_mem():
-    import wmi
-    c = wmi.WMI ()         
-    for processor in c.Win32_Processor(): 
-        print "Process Name: %s" % processor.Name.strip() 
-    for Memory in c.Win32_PhysicalMemory(): 
-        print "Memory Capacity: %.fMB" %(int(Memory.Capacity)/1048576)
-    print "       "
-
-def disk():
-    import wmi
-    c = wmi.WMI ()    
-    for physical_disk in c.Win32_DiskDrive (): 
-        for partition in physical_disk.associators ("Win32_DiskDriveToDiskPartition"): 
-            for logical_disk in partition.associators ("Win32_LogicalDiskToPartition"): 
-                print physical_disk.Caption.encode("UTF8"), partition.Caption.encode("UTF8"), logical_disk.Caption 
-    print "       "
-   
-    for disk in c.Win32_LogicalDisk (DriveType=3): 
-        print disk.Caption, "%0.2f%% free" % (100.0 * long (disk.FreeSpace) / long (disk.Size)) 
-    print "       "
     
 def OverwriteOrNot(Source,Target):
     while(1):
