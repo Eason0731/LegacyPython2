44dcb7ae9cef989c37ec061711d1efdf707202ef
John0731 <john.zhang0731@outlook.com>
2017-07-14 14:23:01 +0800

    Add two addition method to check the title name

--- src/MyItems/Selenium/FirstPythonSeleniumCase.py
+++ src/MyItems/Selenium/FirstPythonSeleniumCase.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 import os
 from selenium import webdriver 
-import time 
+import time
 
 IEdriver = os.path.join(os.path.abspath('.'),'Drivers','IEDriverServer.exe') #Use relative path to get the path of IEDriverServer
 os.environ["webdriver.ie.driver"] = IEdriver #Set a system environment for ie browser then load IEDriverServer file path
@@ -24,10 +24,25 @@ time.sleep(3)
 SearchButton.click()
 time.sleep(3)
 
+print Content + u' - 搜狗搜索 '
 #To check the titile is right
 if Content in driver.title:
     print "Title of search page is right!"
 else:
     print "Current title name is " + driver.title
 
+if Content + u' - 搜狗搜索' == driver.title: #Should add a 'u' before Chinese string
+    print "Title of search page is right!"
+else:
+    print "Current title name is " + driver.title
+
+try:
+    assert Content + u' - 搜狗搜索' in driver.title
+    print "Title of search page is right!"
+except Exception,e:
+    print e
+    print "Current title name is " + driver.title
+    
 driver.quit() #Quit the browser
+
+
