724c62df04fbb5eb7b162abbc84c043eb3cdc69c
John0731 <john.zhang0731@outlook.com>
2017-07-14 10:14:39 +0800

    Update the python selenium case

--- src/MyItems/Selenium/FirstPythonSeleniumCase.py
+++ src/MyItems/Selenium/FirstPythonSeleniumCase.py
@@ -4,12 +4,26 @@ import time
 
 IEdriver = "D:\John's Code Project\MySeleniumCases\Drivers\IEDriverServer.exe"
 os.environ["webdriver.ie.driver"] = IEdriver 
-
-
 driver = webdriver.Ie(IEdriver)
+driver.maximize_window()
 
 driver.get("http://www.sogou.com")
 time.sleep(3)
+Content = "Hello Selenium"
+SearchBox = driver.find_element_by_id("query")
+SearchButton = driver.find_element_by_id("stb")
+time.sleep(3)
+
+SearchBox.send_keys(Content)
+time.sleep(3)
+SearchButton.click()
+time.sleep(3)
+
+try:
+    assert Content in driver.title
+    print "Pass!"
+except Exception as e:
+    print e
 
 print driver.title
 
