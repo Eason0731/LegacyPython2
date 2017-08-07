dbff886aac482fe071e177b40cf69a4c0575f8e3
John0731 <john.zhang0731@outlook.com>
2017-08-07 20:43:24 +0800

    Add BasicAPI folder for Selenium

---
+++ src/MyItems/Selenium/BasicAPI/Getbrowser.py
@@ -0,0 +1,35 @@
+import os
+from selenium import webdriver
+import time
+
+def IE():
+    if os.name == 'nt':
+        IEDriverFile =  os.path.join(os.path.abspath('..'),'Drivers','IEDriverServer.exe')
+        os.environ['webdriver.ie.driver'] = IEDriverFile
+        driver = webdriver.Ie(IEDriverFile)
+        driver.maximize_window()
+    else:
+        print "IE Browser cannot be ran on non-Windows os."
+    return driver
+
+def Chrome():
+    if os.name == 'nt':
+        ChromeDrvier = os.path.join(os.path.abspath('..'),'Drivers','chromedriver.exe')
+    elif os.name == 'posix':
+        ChromeDrvier = os.path.join(os.path.abspath('..'),'Drivers','chromedriver')
+    os.environ['webdriver.chrome.driver'] = ChromeDrvier
+    driver = webdriver.Chrome(ChromeDrvier)
+    driver.maximize_window()
+    return driver
+
+def FireFox():
+    if os.name == 'nt':
+        FireFoxDriver = 'C:\Program Files\Mozilla Firefox'
+        os.environ['PATH'] = FireFoxDriver
+    elif os.name == 'posix':
+        FireFoxDriver = os.path.join(os.path.abspath('..'),'Drivers','geckodriver')
+        os.environ['webdriver.gecko.driver'] = FireFoxDriver
+    driver = webdriver.Firefox(FireFoxDriver)
+    driver.maximize_window()
+    return driver
+
