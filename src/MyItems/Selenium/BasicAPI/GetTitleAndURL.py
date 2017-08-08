8d4270cfeb7b4dce7cfe43f1cfb38aa617e2024c
john0731 <john.zhang0731@outlook.com>
2017-08-08 09:26:45 +0800

    Add some notes on case GetTitleAndURL

--- src/MyItems/Selenium/BasicAPI/GetTitleAndURL.py
+++ src/MyItems/Selenium/BasicAPI/GetTitleAndURL.py
@@ -8,11 +8,11 @@ def GetTitleAndURL():
     driver = Getbrowser.Chrome()
     driver.get('http://www.baidu.com')
     time.sleep(2)
-    if u'百度一下，你就知道' in driver.title:
+    if u'百度一下，你就知道' in driver.title: # "driver.title" to get the page title name on current page
         print "Pass: " + sys._getframe().f_code.co_name
     else:
         print "Current page title is " + driver.title
-    print driver.current_url
+    print driver.current_url # "current_url" to get the page title URL address on current page
     driver.quit()
     
 if __name__ == '__main__':
