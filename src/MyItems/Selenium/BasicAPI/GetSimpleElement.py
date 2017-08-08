be69b27b7e66fc9d80fc1480bb93108acce76b9c
john0731 <john.zhang0731@outlook.com>
2017-08-08 11:38:20 +0800

    Add two get element ways on GetSimpleElement.py

--- src/MyItems/Selenium/BasicAPI/GetSimpleElement.py
+++ src/MyItems/Selenium/BasicAPI/GetSimpleElement.py
@@ -14,6 +14,8 @@ def GetSimpleElement():
     driver.find_element_by_link_text('贴吧') #find link text element
     driver.find_element_by_class_name('c-tips-container') #find class name element
     driver.find_element_by_xpath('//*[@id="kw"]') #find xpath element
+    driver.find_element_by_tag_name('div') #find div element
+    driver.find_element_by_partial_link_text('新') #find part of link text element
     driver.quit()
     
 if __name__ == '__main__':
