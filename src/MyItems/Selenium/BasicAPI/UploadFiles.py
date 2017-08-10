d4229bc6d130a9f0bd2a5e1c991b570445a471f8
John0731 <john.zhang0731@outlook.com>
2017-08-11 07:31:34 +0800

    Update the browser and filepath on case UploadFiles

--- src/MyItems/Selenium/BasicAPI/UploadFiles.py
+++ src/MyItems/Selenium/BasicAPI/UploadFiles.py
@@ -7,11 +7,11 @@ from selenium.webdriver.support.ui import WebDriverWait #Need to import package 
 from selenium.webdriver.common.action_chains import ActionChains #Need to import package ActionChains
 
 def UploadFiles(): #15
-    driver = Getbrowser.Chrome()
+    driver = Getbrowser.IE()
     URL = os.path.join(os.path.abspath('.'),'Html','upload_file.html')
     driver.get(URL)
-    
-    driver.find_element_by_name('file').send_keys('C:\123.txt') #Use send_keys to upload file
+
+    driver.find_element_by_name('file').send_keys(URL) #Use send_keys to upload file
     time.sleep(3)
     driver.quit()
     
