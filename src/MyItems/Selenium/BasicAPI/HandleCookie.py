5d278d428eb55b29c8f30845fb0cc8670afa8254
john0731 <john.zhang0731@outlook.com>
2017-08-11 13:47:30 +0800

    Add a new test case

---
+++ src/MyItems/Selenium/BasicAPI/HandleCookie.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+import os
+import time
+import Getbrowser
+import sys
+from selenium.webdriver.support.ui import WebDriverWait #Need to import package WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains #Need to import package ActionChains
+
+def HandleCookie(): #19
+    driver = Getbrowser.Chrome()
+    URL = 'http://www.youdao.com'
+    driver.get(URL)
+
+    driver.add_cookie({'name':'name_aaaaaa', 'value':'value_bbbb'}) #add_cookie() to add some infos on cookies. Add cookie should before then get all it will be overwrited
+    cookies = driver.get_cookies() #get_cookies() to get cookies from current page
+    for cookie in cookies:
+        print "%s -> %s" % (cookie['name'],cookie['value']) #To get all 'name' and 'value' cookies on current page
+
+    print "=================================================="    
+    print cookies
+
+    driver.delete_cookie('CookieName') #Delete the cookie named CookieName
+    driver.delete_all_cookies() # Can delete all cookies
+
+    time.sleep(3)
+    driver.quit()
+
+if __name__ == '__main__':
+   HandleCookie()
