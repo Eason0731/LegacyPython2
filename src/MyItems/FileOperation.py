e0cabca636dbcbe0b76f12c230194212b4a7800f
john0731 <john.zhang0731@outlook.com>
2017-08-18 17:00:28 +0800

    Fix the issue #28

--- src/MyItems/FileOperation.py
+++ src/MyItems/FileOperation.py
@@ -190,7 +190,9 @@ def Copy(Source):
         else:
             copyFiles(Source,Target)
     elif os.path.isfile(Source):
-        shutil.copy (Source,Target) 
+        shutil.copy (Source,Target)
+    if not '\\' in Target:
+        Target = Target.replace(Target,os.path.join(Target,'\\'))
     print Source + " has copied to " + '\\'.join(Target.split("\\")[:-1]) + " successfully!"
     print time.strftime("End Time :%Y-%m-%d %X",time.localtime())
     print "=================== Finish ========================"
@@ -227,9 +229,9 @@ def copyFiles(sourceDir, targetDir):
 def Move(Source):
     Target = raw_input ("Please input the target folder path: ")
     TargetFolderEmptyOrNot(Source,Target,sys._getframe().f_code.co_name)
+    Type = Source.split("\\")[-1]
     if not os.path.exists(Target):
         os.makedirs(Target)
-    Type = Source.split("\\")[-1]
     Target = os.path.join(Target,Type)
     if os.path.exists(Target):
         if Source == Target:
@@ -250,6 +252,8 @@ def Move(Source):
             copyFiles(Source,Target)                    
         shutil.rmtree(Source)
     if os.path.exists(Target):
+        if not '\\' in Target:
+            Target = Target.replace(Target,os.path.join(Target,'\\'))
         print Source + " has been moved from " + "\\".join(Source.split("\\")[0:-1]) + " to " + "\\".join(Target.split("\\")[0:-1]) + " successfully!"
     else:
         print Source + " has been moved failed!"
